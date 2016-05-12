848e9135ba637d8487ba9073f50e56dc3ae7d848
nicolas-ivanov <nickolas.ivanov@gmail.com>
2016-05-12 17:06:33 +0300

    haversine formula for distance calculation

---
+++ dist_calculation.py
@@ -0,0 +1,20 @@
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+if __name__ == '__main__':
+    print 'Approximate distance between Moscow and Saint Petersburg: %d km' % haversine(55, 37, 59, 30)
