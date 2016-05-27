bf54d10ae943bcaec29a51934b38396e3347bf68
ISosnovik <sosnovikivan@gmail.com>
2016-05-27 12:32:20 +0300

    fix jac

--- text_filters.py
+++ text_filters.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def jaccard_similarity(list1, list2):
     overlap = list(set(list1) & set(list2))
+    if len(overlap) == 0:
+        return 0
     join = list(set(list1 + list2))
     return 1.0 * len(overlap) / len(join)
 
