3ad0923777e2e98c7ae975b968338fa94cb6a611
Pavel Turanchoks <ipuncho@gmail.com>
2016-05-27 10:03:15 +0300

    buttons

--- static/script.js
+++ static/script.js
@@ -5,15 +5,31 @@ angular
         'ngMap'
     ])
     .controller('MainCtrl', function($http, $scope, NgMap) {
+        $scope.data = {};
+
         var test = [
             '617749885933232128', '617749885933232128'
         ];
 
         $scope.tweetPairs = [];
 
-        $scope.formSubmit = function() {
-            $http
-                .get('/ppjoin')
+
+        $scope.search = function() {
+            request({
+                q: $scope.data.theta
+            });
+        }
+
+        $scope.feelingLucky = function() {
+            request();
+        }
+
+
+        function request(params) {
+            return $http
+                .get('/ppjoin', {
+                    params: params
+                })
                 .then(function(response) {
                     $scope.tweetPairs = [test];
                     // $scope.tweetPairs = response.data || [];
@@ -23,6 +39,5 @@ angular
                     console.log(response);
                 });
         }
-
 
     });
